ac1e0fc9e4d60da46e0455c8e9a940ff3cc9ac36
Jim <jim.pollaro@gmail.com>
2023-03-17 09:09:53 -0700

    pre redis

--- hbl/urls.py
+++ hbl/urls.py
@@ -1,9 +1,11 @@
 from django.urls import re_path
 
 from hbl.views.auth_views import CheckLoggedInView, OauthView, RedirectURIView
+from hbl.views.team_views import TeamsView
 
 urlpatterns = [
     re_path(r"^login/?$", OauthView.as_view()),
     re_path(r"^redirect/?$", RedirectURIView.as_view()),
     re_path(r"^check/?$", CheckLoggedInView.as_view()),
+    re_path(r"^teams/?$", TeamsView.as_view()),
 ]
--- hbl/views/team_views.py
+++ hbl/views/team_views.py
@@ -1,1 +1,13 @@
-YAHOO_LEAGUE_API = "https://fantasysports.yahooapis.com/fantasy/v2/league"
+import requests
+from decouple import config
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+
+TEAMS_API = "".join([config("YAHOO_LEAGUE_API"), "/", config("HBL_2023_ID")])
+
+
+class TeamsView(ListAPIView):
+    def get(self, request):
+        access_token = f'Bearer {request.sessions.get("yahoo_access_token")}'
+        resp = requests.get(TEAMS_API, headers={"Authorization": access_token})
+        return Response(resp)
