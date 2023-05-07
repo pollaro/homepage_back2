22e163ee2158b2c1d422e4682aca70a7c5b2412d
Jim <jim.pollaro@gmail.com>
2023-05-07 12:08:03 -0700

    fix setting

--- homepage2_back/production_settings.py
+++ homepage2_back/production_settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.52.122.155"]
 
 
 # Application definition
