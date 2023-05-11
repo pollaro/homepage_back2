a66fd2f434666d78bcde28253a554a033ddb83b8
Jim <jim.pollaro@gmail.com>
2023-05-11 13:16:16 -0700

    add logging to view

--- hbl/views/team_views.py
+++ hbl/views/team_views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 import requests
 import xmltodict
@@ -10,7 +11,9 @@ from rest_framework.views import APIView
 
 from hbl.decorators import auth_required
 from hbl.models import HBLTeam
-from hbl.serializers import ManagerSerializer, TeamSerializer
+from hbl.serializers import TeamSerializer
+
+logger = logging.getLogger(__file__)
 
 
 class TeamsView(APIView):
@@ -24,6 +27,7 @@ class TeamsView(APIView):
             f"{config('YAHOO_LEAGUE_API')}league/{config('HBL_2023_ID')}/teams",
             headers={"Authorization": f'Bearer {cache.get("access_token")}'},
         )
+        logger.info(f"Response from Yahoo is {response_xml.status_code}")
         response_dict = xmltodict.parse(response_xml.text)
         league = response_dict["fantasy_content"]["league"]["teams"]["team"]
         hbl_team_updates = []
@@ -36,8 +40,8 @@ class TeamsView(APIView):
                     "name": team["managers"]["manager"]["nickname"],
                 }
                 team["manager"] = manager
-            team["yahoo_team_key"] = team["team_key"]
-            hbl_team_updates.append(team)
+                team["yahoo_team_key"] = team["team_key"]
+                hbl_team_updates.append(team)
         serializer = TeamSerializer(data=hbl_team_updates, many=True)
         serializer.is_valid()
         serializer.save()
@@ -52,5 +56,6 @@ class TeamDetailView(APIView):
             f'{config("YAHOO_LEAGUE_API")}team/{config("HBL_2023_ID")}.t.{team_id}',
             headers={"Authorization": f"Bearer {cache.get('access_token')}"},
         )
+        logger.info(f"Response from Yahoo is {response_xml.status_code}")
         response_dict = xmltodict.parse(response_xml.text)
         return HttpResponse(json.dumps(response_dict["fantasy_content"]["team"]))
