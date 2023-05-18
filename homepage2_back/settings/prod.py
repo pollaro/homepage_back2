dbbf71ca4a974d86bc022071ab1d0b320ebaabf5
Jim <jim.pollaro@gmail.com>
2023-05-18 15:33:42 -0700

    add allowed host

--- homepage2_back/settings/prod.py
+++ homepage2_back/settings/prod.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "13.52.122.155"]
+ALLOWED_HOSTS = ["localhost", "13.52.122.155", "jimpollaro.com"]
 
 
 # Application definition
